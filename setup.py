79d244257945b22be23a4245e8a706926f4e30c2
Eric Tsai <eric@mbientlab.com>
2017-09-26 19:12:08 -0700

    Creating mock commit for setup.py
    Want to use this version of gattlb rather than the one on pypi

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ else:
 
 setup(
     name='gattlib',
-    version="0.20150805",
+    version="0.20160216",
     description="Library to access Bluetooth LE devices",
     author="Oscar Acena",
     author_email="oscar.acena@gmail.com",
